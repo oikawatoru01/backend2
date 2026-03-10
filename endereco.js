556fb4157fce38d3525ce7743753283fce7e270f
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-03-10 11:41:34 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "Rua das Flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + "," + numero + "-" + cidade;
+//console.log(endereco);
+console.log("O endereco correto é: " + endereco);
