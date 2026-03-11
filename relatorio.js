1e378e90b725416fa2966751fd60600e418dea82
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-03-11 08:47:49 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,9 @@
+//Relatório de notas
+let aluno = "Pedro";
+let nota1 = 7.0;
+let nota2 = 4.0;
+
+let media = (nota1 + nota2) / 2;
+let relatorio = "Aluno: " + aluno + " | Notas: " + nota1 + " e " + nota2 + " | Média de notas:" + media;
+console.log("Relatorio de notas");
+console.log(relatorio);
