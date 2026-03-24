14ef00736987d86760263c9fb2a24d09e2fe1aa1
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-03-24 14:41:05 +0000

    versao1

---
+++ interpolacao.js
@@ -0,0 +1,10 @@
+const nome = "Ana";
+const idade = 27
+const anoAtual = 2026;
+
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10;
+
+console.log("O ano de nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log (`Daqui a 10 anos, terá ${idadeEmDezAnos} anos. `);
