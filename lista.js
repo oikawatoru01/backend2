3825e4da9338f0a37588363f520f191e032f1492
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-04-15 13:45:15 +0000

    versao1

---
+++ lista.js
@@ -0,0 +1,11 @@
+const alunos = ["João", "Carlos Passiva", "Kimberly"];
+
+alunos.push("Diego");
+alunos.unshift("Gaby");
+console.log(alunos);
+
+console.log(alunos.indexOf("Diego"));
+console.log(alunos.indexOf("Gaby"));
+
+console.log(alunos.includes("Diego"));
+console.log(alunos.imcludes("Gaby"));
