790db82b6c2ae5be721d217f18928a88d3920e97
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-04-29 13:53:41 +0000

    versao2

---
+++ sgp.js
@@ -0,0 +1,33 @@
+//SISTEMA DE GERENCIAMENTO DE PERSONAGEM
+
+function calcularPontuacaoTotal(historicoDePontos) {
+    const somarRodada = function(acumulador, pontosDaFase) {
+        const novaSoma = acumulador + pontosDaFase;
+        return novaSoma;
+    };
+
+    const pontuacaoFinal = historicoDePontos.reduce(somarRodada, 0);
+    return pontuacaoFinal;
+};
+
+function filtrarMissoesConcluidas(listaDeMissoes) {
+    const verificarStatusDaMissao = function(missao) {
+        const foiFinalizada = missao.status === "concluída"; //=== igualdade    estrita
+        return foiFinalizada;
+    };
+
+    const apenasConcluidas = listaDeMissoes.filter(verificarStatusDaMissao);
+    return apenasConcluidas;
+};
+
+function atualizarInventario(inventarioAtual, acao, nomeDoItem) {
+    let novoInventario;
+
+    if (acao === "pegar") {
+        const inventarioComItemNovo = [inventarioAtual, nomeDoItem];
+        novoInventario = inventarioComItemNovo;
+    } else if (acao === "descartar") //Parei aqui
+}
+
+const pontosDoJogador = [100, 50, 200, 10];
+console.log(pontosDoJogador);
