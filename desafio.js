1391b8ca64ee76125cd1d8c054eb7a2d02c13d99
oikawatoru01 <julia.vitoria.nunes@escola.pr.gov.br>
2026-05-20 13:55:58 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,13 @@
+let nomeJogador = "Arthur";
+const nomePersonagem = "Harry Mason";
+const nomeJogo = "Silent hill";
+
+let objetivo = "Encontrar sua filha";
+let recompesa = "Sair de silent hill";
+
+console.log(`${nomeJogador} jogou com ${nomePersonagem} no jogo ${nomeJogo} e precisava ${objetivo} para ${recompesa}`);
+
+let balas = 24;
+let inimigos = 12;
+let derrotar = balas - inimigos;
+
